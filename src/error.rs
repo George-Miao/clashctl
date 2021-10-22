6233f5e069913e7aac6aed0bef13ea7d3f0759e6
George-Miao <gm@miao.dev>
2021-10-23 07:50:39 +0800

    feat: Error class

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,13 @@
 #[derive(thiserror::Error, Debug)]
-pub enum Error {}
+pub enum Error {
+    #[error("Invalid URL format")]
+    UrlParseError,
+    #[error("Error whiling requesting API")]
+    RequestError,
+    #[error("Broken response from server")]
+    BadResponseEncoding,
+    #[error("Broken response from server")]
+    BadResponseFormat,
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
