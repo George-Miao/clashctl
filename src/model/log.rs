e68ccef19f0811b37fc45eb58bb19ac756fcd54f
George-Miao <gm@miao.dev>
2021-10-23 07:51:13 +0800

    feat: use serde rename_all

--- src/model/log.rs
+++ src/model/log.rs
@@ -1,14 +1,11 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
 pub enum Level {
-    #[serde(rename = "error")]
     Error,
-    #[serde(rename = "warning")]
     Warning,
-    #[serde(rename = "info")]
     Info,
-    #[serde(rename = "debug")]
     Debug,
 }
 
