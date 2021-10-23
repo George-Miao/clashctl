61d98bf89e3a3bfa39644805bb7e5058d745d564
George-Miao <gm@miao.dev>
2021-10-24 01:56:07 +0800

    feat: render proxy as a list

---
+++ src/cli/proxy_render.rs
@@ -0,0 +1,62 @@
+use std::cmp::Ordering;
+
+use owo_colors::OwoColorize;
+use terminal_size::{terminal_size, Height, Width};
+
+use crate::model::{Proxies, Proxy};
+use crate::Result;
+
+impl Proxies {
+    pub fn render_list(&self, sort: ProxySort) -> Result<()> {
+        let (Width(terminal_width), _) = terminal_size().unwrap_or((Width(70), Height(0)));
+        let mut list = self.iter().collect::<Vec<_>>();
+        sort.sort(&mut list);
+        println!("{:<16}{:<8}{}", "TYPE", "DELAY", "NAME");
+        println!("{:-<1$}", "", terminal_width as usize);
+        for (name, _) in list.into_iter() {
+            let proxy = self.get(name).unwrap();
+            let delay = proxy
+                .history
+                .iter()
+                .next()
+                .and_then(|x| Some(format!("{}", x.delay)))
+                .unwrap_or_else(|| "-".into());
+            let type_name = proxy.proxy_type.to_string();
+            println!("{:<16}{:<8}{}", type_name.green(), delay, name)
+        }
+        Ok(())
+    }
+}
+
+pub enum ProxySort {
+    Type,
+    Name,
+    Delay,
+}
+
+impl ProxySort {
+    pub fn by_type() -> Self {
+        Self::Type
+    }
+
+    pub fn by_name() -> Self {
+        Self::Name
+    }
+
+    pub fn by_delay() -> Self {
+        Self::Delay
+    }
+
+    pub fn sort(&self, proxies: &mut Vec<(&String, &Proxy)>) {
+        proxies.sort_by(|lhs, rhs| match self {
+            Self::Type => lhs.1.proxy_type.cmp(&rhs.1.proxy_type),
+            Self::Name => lhs.0.cmp(&rhs.0),
+            Self::Delay => match (lhs.1.history.iter().next(), rhs.1.history.iter().next()) {
+                (Some(l_history), Some(r_history)) => l_history.cmp(&r_history),
+                (Some(_), None) => Ordering::Greater,
+                (None, Some(_)) => Ordering::Less,
+                _ => Ordering::Equal,
+            },
+        });
+    }
+}
