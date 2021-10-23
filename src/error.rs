a02ecd4ba5d2c93ca2d6b1be9371b13440186d3f
George-Miao <gm@miao.dev>
2021-10-24 07:20:43 +0800

    feat: errors

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,35 @@
-#[cfg(feature = "cli")]
-use requestty::ErrorKind;
-use reqwest::StatusCode;
-
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Invalid URL format")]
     UrlParseError,
+
     #[error("Error whiling requesting API")]
     RequestError(#[from] reqwest::Error),
+
     #[error("Broken response from server")]
     BadResponseEncoding,
+
     #[error("Broken response from server")]
     BadResponseFormat,
+
     #[error("Failed response from server")]
-    FailedResponse(StatusCode),
+    FailedResponse(reqwest::StatusCode),
+
+    #[cfg(feature = "cli")]
+    #[error("Cannot find server")]
+    ServerNotFound,
+
     #[cfg(feature = "cli")]
     #[error("Requestty error")]
-    RequesttyError(#[from] ErrorKind),
+    RequesttyError(#[from] requestty::ErrorKind),
+
+    #[cfg(feature = "cli")]
+    #[error("Config file cannot be read")]
+    ConfigFileIoError(std::io::Error),
+
+    #[cfg(feature = "cli")]
+    #[error("Config file cannot be parsed")]
+    ConfigFileFormatError(#[from] ron::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
