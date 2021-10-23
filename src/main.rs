e629b192d6f48b493f66613efbf8b35bb0583fe1
George-Miao <gm@miao.dev>
2021-10-24 07:21:03 +0800

    feat: verbosity in flags

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,26 @@ use clashctl::cli::{init_logger, Cmd, Opts};
 use clashctl::Result;
 
 use clap::Parser;
+use log::{debug, LevelFilter};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    init_logger();
     let opts = Opts::parse();
+
+    init_logger(match opts.flag.verbose {
+        0 => Some(LevelFilter::Info),
+        1 => Some(LevelFilter::Debug),
+        2 => Some(LevelFilter::Trace),
+        _ => None,
+    });
+
+    debug!("Opts: {:#?}", opts);
 
     match opts.cmd {
         Cmd::Completion(arg) => arg.handle()?,
-        Cmd::Proxy(sub) => sub.handle().await?,
-        Cmd::Server(sub) => sub.handle().await?,
+        Cmd::Proxy(sub) => sub.handle(&opts.flag).await?,
+        Cmd::Server(sub) => sub.handle(&opts.flag).await?,
     }
+
     Ok(())
 }
