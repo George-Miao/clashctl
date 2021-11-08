b7b4894b1b6db6f12cc528dc3eb8192dc50f0e6a
George-Miao <gm@miao.dev>
2021-11-08 17:17:25 +0800

    feat: update model field visibility

---
+++ src/model/connection.rs
@@ -0,0 +1,42 @@
+use chrono::{DateTime, Local};
+use serde::{Deserialize, Serialize};
+
+use crate::model::Rule;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Connections {
+    pub connections: Vec<Connection>,
+    pub download_total: u64,
+    pub upload_total: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Connection {
+    pub id: String,
+    pub upload: u64,
+    pub download: u64,
+    pub metadata: Metadata,
+    pub rule: Rule,
+    pub rule_payload: String,
+    pub start: DateTime<Local>,
+    pub chains: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Metadata {
+    #[serde(rename = "type")]
+    pub connection_type: String,
+
+    #[serde(rename = "sourceIP")]
+    pub source_ip: String,
+    pub source_port: String,
+
+    #[serde(rename = "destinationIP")]
+    pub destination_ip: String,
+    pub destination_port: String,
+    pub host: String,
+    pub network: String,
+}
