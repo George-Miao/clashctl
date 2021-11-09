49943c6c610523c51b98cd1f62735e276e5128b2
George-Miao <gm@miao.dev>
2021-11-10 02:58:44 +0800

    feat: add custom logger for TUI

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-use clashctl::cli::{init_logger, Cmd, Opts, TuiOpt};
+use clashctl::cli::{init_logger, Cmd, Flags, Opts, TuiOpt};
 
 use clap::Parser;
 use log::{debug, warn, LevelFilter};
 
 fn main() {
     if std::env::args().len() == 1 {
-        TuiOpt::default().run()
+        TuiOpt::default().run(&Flags::default())
     } else {
         let opts = Opts::parse();
-        init_logger(match opts.flag.verbose {
-            0 => Some(LevelFilter::Info),
-            1 => Some(LevelFilter::Debug),
-            2 => Some(LevelFilter::Trace),
-            _ => None,
-        });
+        if let Cmd::Tui(opt) = opts.cmd {
+            opt.run(&opts.flag)
+        } else {
+            init_logger(match opts.flag.verbose {
+                0 => Some(LevelFilter::Info),
+                1 => Some(LevelFilter::Debug),
+                2 => Some(LevelFilter::Trace),
+                _ => None,
+            });
 
-        debug!("Opts: {:#?}", opts);
+            debug!("Opts: {:#?}", opts);
 
-        match opts.cmd {
-            Cmd::Tui(opt) => opt.run(),
-            Cmd::Completion(arg) => arg.handle(),
-            Cmd::Proxy(sub) => sub.handle(&opts.flag),
-            Cmd::Server(sub) => sub.handle(&opts.flag),
+            match opts.cmd {
+                Cmd::Proxy(sub) => sub.handle(&opts.flag),
+                Cmd::Server(sub) => sub.handle(&opts.flag),
+                Cmd::Completion(arg) => arg.handle(),
+                _ => unreachable!(),
+            }
         }
     }
     .unwrap_or_else(|e| warn!("{}", e))
