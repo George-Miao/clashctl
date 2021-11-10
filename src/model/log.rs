4052e30541fd55c73910470a62cb28481e628d69
George-Miao <gm@miao.dev>
2021-11-10 08:58:07 +0800

    feat: add strum for all enum in models

--- src/model/log.rs
+++ src/model/log.rs
@@ -2,6 +2,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "lowercase")]
+#[cfg_attr(
+    feature = "cli",
+    derive(strum::EnumString, strum::Display, strum::EnumVariantNames),
+    strum(ascii_case_insensitive, serialize_all = "lowercase")
+)]
 pub enum Level {
     Error,
     Warning,
--- src/model/rule.rs
+++ src/model/rule.rs
@@ -1,6 +1,11 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[cfg_attr(
+    feature = "cli",
+    derive(strum::EnumString, strum::Display, strum::EnumVariantNames),
+    strum(ascii_case_insensitive, serialize_all = "lowercase")
+)]
 pub enum Rule {
     Domain,
     DomainSuffix,
