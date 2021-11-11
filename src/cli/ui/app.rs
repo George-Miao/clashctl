9266da14a31fce8601a0d0365319e6eb4c764b1d
George-Miao <gm@miao.dev>
2021-11-12 07:05:23 +0800

    perf: back to single thread so no heavy data race

--- src/cli/ui/app.rs
+++ src/cli/ui/app.rs
@@ -1,9 +1,9 @@
-use std::sync::mpsc::{channel, Sender, TryRecvError};
+use std::io::{self, Stdout};
 use std::sync::Mutex;
 use std::time::Duration;
 use std::{
-    io::{self, Stdout},
-    sync::Arc,
+    sync::mpsc::{channel, Sender, TryRecvError},
+    thread::spawn,
 };
 
 use clap::Parser;
@@ -23,7 +23,7 @@ use crate::cli::ui::{
 };
 use crate::cli::{components::*, ui::pages::LogPage, DiagnosticEvent, Event, Flags};
 use crate::cli::{ui::utils::Interval, TuiStates};
-use crate::{Error, Result};
+use crate::Result;
 
 type Backend = CrosstermBackend<Stdout>;
 
@@ -39,12 +39,8 @@ pub struct TuiOpt {
 }
 
 impl TuiOpt {
-    pub fn into_app(self) -> Result<TuiApp> {
-        Ok(TuiApp::from_opt(self))
-    }
-
     pub fn run(self, flag: &Flags) -> Result<()> {
-        self.into_app()?.run(flag)
+        main_loop(self, flag)
     }
 }
 
@@ -54,124 +50,90 @@ impl Default for TuiOpt {
     }
 }
 
-#[derive(Debug, Default)]
-pub struct TuiApp {
-    opt: TuiOpt,
+fn setup() -> Result<Terminal<Backend>> {
+    let mut stdout = io::stdout();
+
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    enable_raw_mode()?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    Ok(terminal)
 }
 
-impl TuiApp {
-    pub fn from_opt(opt: TuiOpt) -> Self {
-        Self { opt }
-    }
+// TODO: wrap up
+fn wrap_up(mut terminal: Terminal<Backend>) -> Result<()> {
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
 
-    fn setup() -> Result<Terminal<Backend>> {
-        let mut stdout = io::stdout();
+    disable_raw_mode()?;
 
-        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-        enable_raw_mode()?;
+    Ok(())
+}
 
-        let backend = CrosstermBackend::new(stdout);
-        let mut terminal = Terminal::new(backend)?;
-        terminal.clear()?;
+pub fn main_loop(opt: TuiOpt, flag: &Flags) -> Result<()> {
+    if flag.get_config()?.using_server().is_none() {
+        eprintln!("No server configured yet. Use `clashctl server add` first.");
+        return Ok(());
+    };
 
-        Ok(terminal)
-    }
+    let mut terminal = setup()?;
 
-    // TODO: wrap up
-    fn wrap_up(mut terminal: Terminal<Backend>) -> Result<()> {
-        execute!(
-            terminal.backend_mut(),
-            LeaveAlternateScreen,
-            DisableMouseCapture
-        )?;
+    let (tx, rx) = channel();
+    let flag_clone = flag.clone();
+    spawn(move || servo(tx, &opt, &flag_clone));
 
-        disable_raw_mode()?;
-
-        Ok(())
-    }
-
-    pub fn run(&mut self, flag: &Flags) -> Result<()> {
-        match flag.get_config()?.using_server() {
-            Some(server) => server.to_owned(),
-            None => {
-                eprintln!("No server configured yet. Use `clashctl server add` first.");
-                return Ok(());
+    let mut state = TuiStates::new();
+    let mut interval = Interval::every(Duration::from_millis(50));
+    loop {
+        interval.tick();
+        terminal.draw(|f| render(&state, f)).unwrap();
+        state.new_tick();
+        match rx.try_recv() {
+            Ok(event) => match event {
+                Event::Quit => break,
+                _ => state.handle(event)?,
+            },
+            Err(TryRecvError::Disconnected) => {
+                eprintln!("All backend TX dropped");
+                break;
             }
-        };
-
-        let opt = self.opt.clone();
-        let flag = flag.clone();
-
-        let mut terminal = Self::setup()?;
-
-        match crossbeam::scope(move |s| {
-            let (tx, rx) = channel();
-            let state = Arc::new(Mutex::new(TuiStates::new()));
-            let event_state = state.clone();
-
-            s.spawn(move |_| servo(tx, &opt, &flag));
-            s.spawn(move |_| loop {
-                let mut state = event_state.lock().unwrap();
-
-                match rx.try_recv() {
-                    Ok(event) => match event {
-                        Event::Quit => break,
-                        _ => {
-                            state.handle(event);
-                        }
-                    },
-                    Err(TryRecvError::Disconnected) => {
-                        eprintln!("All backend TX dropped");
-                        break;
-                    }
-                    _ => {}
-                }
-            });
-            s.spawn(move |_| {
-                let mut interval = Interval::every(Duration::from_millis(10));
-
-                loop {
-                    interval.tick();
-                    let mut state = match state.try_lock() {
-                        Ok(state) => state,
-                        Err(_) => continue,
-                    };
-                    state.new_tick();
-                    terminal.draw(|f| Self::render(&mut state, f)).unwrap();
-                }
-            });
-        }) {
-            Err(e) => {
-                eprintln!("{:?}", e);
-                Err(Error::TuiInterupttedErr)
-            }
-            Ok(_) => Ok(()),
+            _ => {}
         }
     }
 
-    fn route(state: &TuiStates, area: Rect, f: &mut Frame<Backend>) {
-        match state.page_index {
-            0 => (f.render_widget(StatusPage::new(state), area)),
-            1 => (f.render_widget(ProxiesPage::new(state), area)),
-            2 => (f.render_widget(LogPage::new(state), area)),
-            3 => (f.render_widget(ConfigPage::new(state), area)),
-            4 => (f.render_widget(DebugPage::new(state), area)),
-            _ => unreachable!(),
-        };
-    }
+    wrap_up(terminal)?;
 
-    fn render(state: &TuiStates, f: &mut Frame<Backend>) {
-        let layout = Layout::default()
-            .constraints([Constraint::Length(3), Constraint::Min(0)])
-            .split(f.size());
+    Ok(())
+}
 
-        let tabs = Tabs::new(state);
-        f.render_widget(tabs, layout[0]);
+fn route(state: &TuiStates, area: Rect, f: &mut Frame<Backend>) {
+    match state.page_index {
+        0 => (f.render_widget(StatusPage::new(state), area)),
+        1 => (f.render_widget(ProxiesPage::new(state), area)),
+        2 => (f.render_widget(LogPage::new(state), area)),
+        3 => (f.render_widget(ConfigPage::new(state), area)),
+        4 => (f.render_widget(DebugPage::new(state), area)),
+        _ => unreachable!(),
+    };
+}
 
-        let main = layout[1];
+fn render(state: &TuiStates, f: &mut Frame<Backend>) {
+    let layout = Layout::default()
+        .constraints([Constraint::Length(3), Constraint::Min(0)])
+        .split(f.size());
 
-        Self::route(state, main, f);
-    }
+    let tabs = Tabs::new(state);
+    f.render_widget(tabs, layout[0]);
+
+    let main = layout[1];
+
+    route(state, main, f);
 }
 
 pub struct Logger {
