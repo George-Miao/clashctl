fb743445fcb9ed9bbdb10944841cd07e29a10e25
George-Miao <gm@miao.dev>
2021-11-12 06:31:29 +0800

    feat: separate render and event handler thread

--- src/cli/ui/app.rs
+++ src/cli/ui/app.rs
@@ -1,8 +1,10 @@
-use std::io::{self, Stdout};
 use std::sync::mpsc::{channel, Sender, TryRecvError};
 use std::sync::Mutex;
-use std::thread::spawn;
 use std::time::Duration;
+use std::{
+    io::{self, Stdout},
+    sync::Arc,
+};
 
 use clap::Parser;
 use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
@@ -37,7 +39,7 @@ pub struct TuiOpt {
 }
 
 impl TuiOpt {
-    pub fn into_app<'a>(self) -> Result<TuiApp<'a>> {
+    pub fn into_app(self) -> Result<TuiApp> {
         Ok(TuiApp::from_opt(self))
     }
 
@@ -52,18 +54,14 @@ impl Default for TuiOpt {
     }
 }
 
-#[derive(Clone, Debug, Default)]
-pub struct TuiApp<'a> {
+#[derive(Debug, Default)]
+pub struct TuiApp {
     opt: TuiOpt,
-    state: TuiStates<'a>,
 }
 
-impl<'a> TuiApp<'a> {
+impl TuiApp {
     pub fn from_opt(opt: TuiOpt) -> Self {
-        Self {
-            opt,
-            state: TuiStates::new(),
-        }
+        Self { opt }
     }
 
     fn setup() -> Result<Terminal<Backend>> {
@@ -79,6 +77,7 @@ impl<'a> TuiApp<'a> {
         Ok(terminal)
     }
 
+    // TODO: wrap up
     fn wrap_up(mut terminal: Terminal<Backend>) -> Result<()> {
         execute!(
             terminal.backend_mut(),
@@ -100,88 +99,78 @@ impl<'a> TuiApp<'a> {
             }
         };
 
-        let (tx, rx) = channel();
         let opt = self.opt.clone();
         let flag = flag.clone();
-        let servo_handle = spawn(move || servo(tx, &opt, &flag));
 
         let mut terminal = Self::setup()?;
 
-        let mut interval = Interval::every(Duration::from_millis(10));
+        match crossbeam::scope(move |s| {
+            let (tx, rx) = channel();
+            let state = Arc::new(Mutex::new(TuiStates::new()));
+            let event_state = state.clone();
 
-        loop {
-            interval.tick();
-            self.state.new_tick();
-            terminal.draw(|f| self.render(f))?;
-            if !servo_handle.is_running() {
-                Self::wrap_up(terminal)?;
-                eprintln!("Servo dropped: {:?}", servo_handle.join().unwrap());
-                break;
-            }
-            match rx.try_recv() {
-                Ok(event) => {
-                    if let Err(e) = self.handle(event) {
-                        Self::wrap_up(terminal)?;
-                        eprintln!("Quit: {}", e);
+            s.spawn(move |_| servo(tx, &opt, &flag));
+            s.spawn(move |_| loop {
+                let mut state = event_state.lock().unwrap();
+
+                match rx.try_recv() {
+                    Ok(event) => match event {
+                        Event::Quit => break,
+                        _ => {
+                            state.handle(event);
+                        }
+                    },
+                    Err(TryRecvError::Disconnected) => {
+                        eprintln!("All backend TX dropped");
                         break;
                     }
+                    _ => {}
                 }
-                Err(TryRecvError::Disconnected) => {
-                    Self::wrap_up(terminal)?;
-                    eprintln!("All backend TX dropped");
-                    break;
-                }
-                _ => {}
-            }
-        }
+            });
+            s.spawn(move |_| {
+                let mut interval = Interval::every(Duration::from_millis(10));
 
-        Ok(())
+                loop {
+                    interval.tick();
+                    let mut state = match state.try_lock() {
+                        Ok(state) => state,
+                        Err(_) => continue,
+                    };
+                    state.new_tick();
+                    terminal.draw(|f| Self::render(&mut state, f)).unwrap();
+                }
+            });
+        }) {
+            Err(e) => {
+                eprintln!("{:?}", e);
+                Err(Error::TuiInterupttedErr)
+            }
+            Ok(_) => Ok(()),
+        }
     }
 
-    fn route(&mut self, area: Rect, f: &mut Frame<Backend>) {
-        match self.state.page_index {
-            0 => {
-                let page = StatusPage::default();
-                f.render_stateful_widget(page, area, &mut self.state)
-            }
-            1 => {
-                let page = ProxiesPage::default();
-                f.render_stateful_widget(page, area, &mut self.state)
-            }
-            2 => {
-                let page = LogPage::default();
-                f.render_stateful_widget(page, area, &mut self.state)
-            }
-            3 => {
-                let page = ConfigPage::default();
-                f.render_stateful_widget(page, area, &mut self.state)
-            }
-            4 => {
-                let page = DebugPage::default();
-                f.render_stateful_widget(page, area, &mut self.state)
-            }
+    fn route(state: &TuiStates, area: Rect, f: &mut Frame<Backend>) {
+        match state.page_index {
+            0 => (f.render_widget(StatusPage::new(state), area)),
+            1 => (f.render_widget(ProxiesPage::new(state), area)),
+            2 => (f.render_widget(LogPage::new(state), area)),
+            3 => (f.render_widget(ConfigPage::new(state), area)),
+            4 => (f.render_widget(DebugPage::new(state), area)),
             _ => unreachable!(),
-        }
+        };
     }
 
-    fn render(&mut self, f: &mut Frame<Backend>) {
+    fn render(state: &TuiStates, f: &mut Frame<Backend>) {
         let layout = Layout::default()
             .constraints([Constraint::Length(3), Constraint::Min(0)])
             .split(f.size());
 
-        let tabs = Tabs::default();
-        f.render_stateful_widget(tabs, layout[0], &mut self.state);
+        let tabs = Tabs::new(state);
+        f.render_widget(tabs, layout[0]);
 
         let main = layout[1];
 
-        self.route(main, f);
-    }
-
-    fn handle(&mut self, event: Event) -> Result<()> {
-        match event {
-            Event::Quit => Err(Error::TuiInterupttedErr),
-            _ => self.state.handle(event),
-        }
+        Self::route(state, main, f);
     }
 }
 
