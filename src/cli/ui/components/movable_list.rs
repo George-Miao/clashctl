4eea726c7e99f490dfdbb851e06605807e0adac7
George-Miao <gm@miao.dev>
2021-11-11 12:25:40 +0800

    feat: use custom render trait for custom impl

--- src/cli/ui/components/movable_list.rs
+++ src/cli/ui/components/movable_list.rs
@@ -1,36 +1,62 @@
+use std::marker::PhantomData;
+
 use tui::widgets::List;
 use tui::{
     text::Spans,
-    widgets::{ListItem, StatefulWidget, Widget},
+    widgets::{ListItem, Widget},
 };
+use unicode_width::UnicodeWidthStr;
 
 use crate::cli::{
-    components::{get_block, get_focused_block, get_text_style, spans_window},
+    components::{
+        get_block, get_focused_block, get_text_style, spans_window, GenericStatefulWidget,
+    },
     Coord,
 };
 
 #[derive(Clone, Debug)]
-pub struct MovableList<'a> {
+pub struct MovableList<'a, T> {
     title: String,
-    pub items: Vec<Spans<'a>>,
+    _life: PhantomData<&'a T>,
 }
 
-impl<'a> MovableList<'a> {
-    pub fn new<C: Into<Spans<'a>>, T: Into<String>>(items: Vec<C>, title: T) -> Self {
+impl<'a, T> MovableList<'a, T>
+where
+    T: Into<Spans<'a>>,
+{
+    pub fn new<TITLE: Into<String>>(title: TITLE) -> Self {
         Self {
-            items: items.into_iter().map(|x| x.into()).collect(),
             title: title.into(),
+            _life: PhantomData,
         }
     }
 }
 
-#[derive(Default, Clone, Debug)]
-pub struct MovableListState {
-    pub offset: Coord,
+#[derive(Debug)]
+pub struct MovableListState<'a, T>
+where
+    T: Into<Spans<'a>>,
+{
+    pub offset: &'a mut Coord,
+    pub items: Vec<T>,
+    _life: PhantomData<&'a T>,
 }
 
-impl<'a> StatefulWidget for MovableList<'a> {
-    type State = MovableListState;
+impl<'a, T> MovableListState<'a, T>
+where
+    T: Into<Spans<'a>>,
+{
+    pub fn new(items: Vec<T>, offset: &'a mut Coord) -> Self {
+        Self {
+            offset,
+            items,
+            _life: PhantomData,
+        }
+    }
+}
+
+impl<'a> GenericStatefulWidget<String> for MovableList<'a, String> {
+    type State = MovableListState<'a, String>;
     fn render(
         self,
         area: tui::layout::Rect,
@@ -38,30 +64,40 @@ impl<'a> StatefulWidget for MovableList<'a> {
         state: &mut Self::State,
     ) {
         let height = (area.height as usize).saturating_sub(2);
-        let num = self.items.len();
+        let num = state.items.len();
 
+        // Calculate which portion of the list will be displayed
         let y_offset = if height + state.offset.y > num {
             num.saturating_sub(height)
         } else {
             state.offset.y
         };
 
+        // Get that portion of items
+        let items = state
+            .items
+            .iter()
+            .rev()
+            .skip(y_offset)
+            .take(area.height as usize)
+            .clone();
+
+        // Calculate what is current x offset
+        // A.K.A. which part will be hidden
         let x_offset = state.offset.x.min(
-            self.items
-                .iter()
+            items
+                .clone()
                 .map(|x| x.width())
                 .min()
                 .unwrap_or_default()
                 .saturating_sub(1),
         );
 
-        let items = self
-            .items
-            .into_iter()
-            .rev()
-            .skip(y_offset)
-            .take(area.height as usize);
+        let items = items.map(|x| -> Spans { x.split_at(x_offset).1.into() });
 
+        // Limit how many chars will be hidden overall
+        // Apply offsets back so the offset is being limited to current one
+        // Even for next tick
         state.offset.x = x_offset;
         state.offset.y = y_offset;
 
@@ -71,21 +107,81 @@ impl<'a> StatefulWidget for MovableList<'a> {
             get_block(&self.title)
         };
 
+        // Spans window is expensive
+        // However it is needed to display part of spans while keeping its style
+        let items = List::new(items.map(ListItem::new).collect::<Vec<_>>())
+            .block(block)
+            .style(get_text_style());
+
+        Widget::render(items, area, buf)
+    }
+}
+
+impl<'a> GenericStatefulWidget<Spans<'a>> for MovableList<'a, Spans<'a>> {
+    type State = MovableListState<'a, Spans<'a>>;
+    fn render(
+        self,
+        area: tui::layout::Rect,
+        buf: &mut tui::buffer::Buffer,
+        state: &mut Self::State,
+    ) {
+        let height = (area.height as usize).saturating_sub(2);
+        let num = state.items.len();
+
+        // Calculate which portion of the list will be displayed
+        let y_offset = if height + state.offset.y > num {
+            num.saturating_sub(height)
+        } else {
+            state.offset.y
+        };
+
+        // Get that portion of items
+        let items = state
+            .items
+            .iter()
+            .rev()
+            .skip(y_offset)
+            .take(area.height as usize)
+            .to_owned();
+
+        // Calculate what is current x offset
+        // A.K.A. which part will be hidden
+        let x_offset = state.offset.x.min(
+            items
+                .clone()
+                .map(|x| x.width())
+                .min()
+                .unwrap_or_default()
+                .saturating_sub(1),
+        );
+
+        // Limit how many chars will be hidden overall
+        // Apply offsets back so the offset is being limited to current one
+        // Even for next tick
+        state.offset.x = x_offset;
+        state.offset.y = y_offset;
+
+        let block = if state.offset.hold {
+            get_focused_block(&self.title)
+        } else {
+            get_block(&self.title)
+        };
+
+        // Spans window is expensive
+        // However it is needed to display part of spans while keeping its style
         let items = List::new(
             items
-                .map(|x| ListItem::new(spans_window(x, x_offset..x_offset + area.width as usize)))
+                .map(|x| {
+                    ListItem::new(spans_window(
+                        x.to_owned(),
+                        x_offset..x_offset + area.width as usize,
+                    ))
+                })
                 .collect::<Vec<_>>(),
         )
         .block(block)
         .style(get_text_style());
 
         Widget::render(items, area, buf)
-    }
-}
-
-impl<'a> Widget for MovableList<'a> {
-    fn render(self, area: tui::layout::Rect, buf: &mut tui::buffer::Buffer) {
-        let mut state = MovableListState::default();
-        StatefulWidget::render(self, area, buf, &mut state)
     }
 }
