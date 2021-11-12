c558aa721fb3fc3b3450977a93cee8c4f902bcb8
George-Miao <gm@miao.dev>
2021-11-12 16:34:55 +0800

    feat(WIP): proxy page

--- src/cli/ui/components/proxy.rs
+++ src/cli/ui/components/proxy.rs
@@ -1,17 +1,14 @@
-use std::marker::PhantomData;
+use std::{borrow::Cow, fmt::Debug, marker::PhantomData};
 
 use tui::{
     style::{Color, Modifier, Style},
-    text::{Span, Spans, Text},
+    text::{Span, Spans},
     widgets::{Paragraph, Widget},
 };
 
 use crate::{
-    cli::{
-        components::{get_block, get_focused_block},
-        Coord,
-    },
-    model::{Proxies, Proxy, ProxyType},
+    cli::components::{get_block, get_focused_block, get_text_style},
+    model::{History, Proxies, Proxy, ProxyType},
 };
 
 #[derive(Clone, Debug)]
@@ -20,14 +17,145 @@ pub struct ProxyGroup<'a> {
     pub proxy_type: ProxyType,
     pub members: Vec<ProxyItem>,
     pub current: Option<usize>,
-    pub folded: bool,
     _life: PhantomData<&'a ()>,
 }
 
-impl<'a> ProxyGroup<'a> {
-    pub(crate) fn get_widget(&self) -> Text<'a> {
-        let delimiter = Span::raw(" ");
+struct Consts {}
 
+impl Consts {
+    pub const PROXY_LATENCY_SIGN: &'static str = "⬤ ";
+
+    pub const NOT_PROXY_SIGN: &'static str = "✪ ";
+
+    pub const NO_LATENCY_SIGN: &'static str = "⊝";
+
+    pub const FOCUSED_INDICATOR: &'static str = "🮇  ";
+
+    pub const FOCUSED_EXPANDED_INDICATOR: &'static str = "🮇   ";
+
+    pub const UNFOCUSED_INDICATOR: &'static str = "   ";
+
+    pub const UNFOCUSED_EXPANDED_INDICATOR: &'static str = "    ";
+
+    pub const DEFAULT_STYLE: Style = Style {
+        add_modifier: Modifier::empty(),
+        sub_modifier: Modifier::empty(),
+        fg: None,
+        bg: None,
+    };
+
+    pub const PROXY_TYPE_STYLE: Style = Style {
+        fg: Some(Color::Gray),
+        add_modifier: Modifier::DIM,
+        ..Self::DEFAULT_STYLE
+    };
+
+    pub const NO_LATENCY_STYLE: Style = Style {
+        fg: Some(Color::DarkGray),
+        ..Self::DEFAULT_STYLE
+    };
+
+    pub const LOW_LATENCY_STYLE: Style = Style {
+        fg: Some(Color::LightGreen),
+        ..Self::DEFAULT_STYLE
+    };
+
+    pub const MID_LATENCY_STYLE: Style = Style {
+        fg: Some(Color::LightYellow),
+        ..Self::DEFAULT_STYLE
+    };
+
+    pub const HIGH_LATENCY_STYLE: Style = Style {
+        fg: Some(Color::LightRed),
+        ..Self::DEFAULT_STYLE
+    };
+
+    pub const DELIMITER_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(" "),
+        style: Self::DEFAULT_STYLE,
+    };
+
+    pub const NOT_PROXY_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::NOT_PROXY_SIGN),
+        style: Self::NO_LATENCY_STYLE,
+    };
+
+    pub const NO_LATENCY_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::PROXY_LATENCY_SIGN),
+        style: Self::NO_LATENCY_STYLE,
+    };
+
+    pub const LOW_LATENCY_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::PROXY_LATENCY_SIGN),
+        style: Self::LOW_LATENCY_STYLE,
+    };
+
+    pub const MID_LATENCY_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::PROXY_LATENCY_SIGN),
+        style: Self::MID_LATENCY_STYLE,
+    };
+
+    pub const HIGH_LATENCY_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::PROXY_LATENCY_SIGN),
+        style: Self::HIGH_LATENCY_STYLE,
+    };
+
+    pub const FOCUSED_INDICATOR_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::FOCUSED_INDICATOR),
+        style: Style {
+            fg: Some(Color::LightYellow),
+            ..Self::DEFAULT_STYLE
+        },
+    };
+
+    pub const FOCUSED_EXPANDED_INDICATOR_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::FOCUSED_EXPANDED_INDICATOR),
+        style: Style {
+            fg: Some(Color::LightYellow),
+            ..Self::DEFAULT_STYLE
+        },
+    };
+
+    pub const UNFOCUSED_INDICATOR_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::UNFOCUSED_INDICATOR),
+        style: Self::DEFAULT_STYLE,
+    };
+
+    pub const UNFOCUSED_EXPANDED_INDICATOR_SPAN: Span<'static> = Span {
+        content: Cow::Borrowed(Self::UNFOCUSED_EXPANDED_INDICATOR),
+        style: Style {
+            fg: Some(Color::LightYellow),
+            ..Self::DEFAULT_STYLE
+        },
+    };
+}
+
+impl<'a> ProxyGroup<'a> {
+    pub(crate) fn get_summary_widget(&self) -> impl Iterator<Item = Span> {
+        self.members.iter().map(|x| {
+            if x.proxy_type.is_normal() {
+                match x.history {
+                    Some(History { delay, .. }) => Self::get_delay_span(delay),
+                    None => Consts::NO_LATENCY_SPAN,
+                }
+            } else {
+                Consts::NOT_PROXY_SPAN
+            }
+        })
+    }
+
+    pub(crate) fn get_widget(
+        &'a self,
+        width: usize,
+        expanded: bool,
+        focused: bool,
+    ) -> Vec<Spans<'a>> {
+        let delimiter = Span::raw(" ");
+        let prefix = if focused {
+            Consts::FOCUSED_INDICATOR_SPAN
+        } else {
+            Consts::UNFOCUSED_INDICATOR_SPAN
+        };
         let name = Span::styled(
             self.name.to_owned(),
             Style::default()
@@ -35,27 +163,117 @@ impl<'a> ProxyGroup<'a> {
                 .add_modifier(Modifier::BOLD),
         );
 
-        let proxy_type = Span::styled(
-            self.proxy_type.to_string(),
-            Style::default().fg(Color::Gray).add_modifier(Modifier::DIM),
-        );
+        let proxy_type = Span::styled(self.proxy_type.to_string(), Consts::PROXY_TYPE_STYLE);
 
         let proxy_count = Span::styled(
             self.members.len().to_string(),
-            Style::default()
-                .fg(Color::Blue)
-                .add_modifier(Modifier::REVERSED),
+            Style::default().fg(Color::Green),
         );
 
-        let first_row = Spans::from(vec![
+        let mut ret = Vec::with_capacity(if expanded { self.members.len() + 1 } else { 2 });
+
+        ret.push(Spans::from(vec![
+            prefix.clone(),
             name,
             delimiter.clone(),
             proxy_type,
             delimiter,
             proxy_count,
-        ]);
+        ]));
 
-        Text::from(vec![first_row])
+        if expanded {
+            let expand_prefix = if focused {
+                Consts::FOCUSED_EXPANDED_INDICATOR_SPAN
+            } else {
+                Consts::UNFOCUSED_EXPANDED_INDICATOR_SPAN
+            };
+
+            let text_style = get_text_style();
+            let is_current = |index: usize| self.current.map(|x| x == index).unwrap_or(false);
+
+            ret.extend(self.members.iter().enumerate().map(|(i, x)| {
+                let name = Span::styled(
+                    &x.name,
+                    if is_current(i) {
+                        Style::default()
+                            .fg(Color::Blue)
+                            .add_modifier(Modifier::BOLD)
+                    } else {
+                        text_style
+                    },
+                );
+                let proxy_type = Span::styled(x.proxy_type.to_string(), Consts::PROXY_TYPE_STYLE);
+
+                let delay_span = x
+                    .history
+                    .as_ref()
+                    .map(|x| {
+                        if x.delay > 0 {
+                            let style = Self::get_delay_style(x.delay);
+                            Span::styled(x.delay.to_string(), style)
+                        } else {
+                            Span::styled(Consts::NO_LATENCY_SIGN, Consts::NO_LATENCY_STYLE)
+                        }
+                    })
+                    .unwrap_or_else(|| {
+                        if !x.proxy_type.is_normal() {
+                            Span::raw("")
+                        } else {
+                            Span::styled(Consts::NO_LATENCY_SIGN, Consts::NO_LATENCY_STYLE)
+                        }
+                    });
+                vec![
+                    expand_prefix.clone(),
+                    Consts::DELIMITER_SPAN.clone(),
+                    name,
+                    Consts::DELIMITER_SPAN.clone(),
+                    proxy_type,
+                    Consts::DELIMITER_SPAN.clone(),
+                    delay_span,
+                ]
+                .into()
+            }))
+        } else {
+            ret.extend(
+                self.get_summary_widget()
+                    .collect::<Vec<_>>()
+                    .chunks(
+                        width
+                            .saturating_sub(Consts::FOCUSED_INDICATOR_SPAN.width() + 2)
+                            .saturating_div(2),
+                    )
+                    .map(|x| {
+                        std::iter::once(if focused {
+                            Consts::FOCUSED_INDICATOR_SPAN
+                        } else {
+                            Consts::UNFOCUSED_INDICATOR_SPAN
+                        })
+                        .chain(x.to_owned().into_iter())
+                        .collect::<Vec<_>>()
+                        .into()
+                    }),
+            )
+        }
+
+        ret
+    }
+
+    fn get_delay_style(delay: u64) -> Style {
+        match delay {
+            0 => Consts::NO_LATENCY_STYLE,
+            1..=200 => Consts::LOW_LATENCY_STYLE,
+            201..=400 => Consts::MID_LATENCY_STYLE,
+            401.. => Consts::HIGH_LATENCY_STYLE,
+        }
+    }
+
+    fn get_delay_span(delay: u64) -> Span<'static> {
+        match delay {
+            0 => Consts::NO_LATENCY_SPAN,
+            1..=200 => Consts::LOW_LATENCY_SPAN,
+            201..=400 => Consts::MID_LATENCY_SPAN,
+            401.. => Consts::HIGH_LATENCY_SPAN,
+        }
     }
 }
 
@@ -66,7 +284,6 @@ impl<'a> Default for ProxyGroup<'a> {
             current: None,
             proxy_type: ProxyType::Selector,
             name: String::new(),
-            folded: true,
             _life: PhantomData,
         }
     }
@@ -76,30 +293,46 @@ impl<'a> Default for ProxyGroup<'a> {
 pub struct ProxyItem {
     pub name: String,
     pub proxy_type: ProxyType,
+    pub history: Option<History>,
     pub udp: bool,
 }
 
 impl<'a> From<(&'a str, &'a Proxy)> for ProxyItem {
     fn from(val: (&'a str, &'a Proxy)) -> Self {
+        let (name, proxy) = val;
         Self {
-            name: val.0.to_owned(),
-            proxy_type: val.1.proxy_type,
-            udp: val.1.udp,
+            name: name.to_owned(),
+            proxy_type: proxy.proxy_type,
+            history: proxy.history.get(0).cloned(),
+            udp: proxy.udp,
         }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Default)]
+pub struct ProxyExpandState {
+    pub cursor: usize,
+    pub expanded: bool,
+}
+
+impl ProxyExpandState {
+    pub fn toggle(&mut self) {
+        self.expanded = !self.expanded
     }
 }
 
 #[derive(Clone, Debug, Default)]
 pub struct ProxyTree<'a> {
     pub groups: Vec<ProxyGroup<'a>>,
-    pub offset: Coord,
+    pub expand_state: ProxyExpandState,
+    pub cursor: usize,
 }
 
 impl<'a> From<Proxies> for ProxyTree<'a> {
     fn from(val: Proxies) -> Self {
         let mut ret = Self {
             groups: Vec::with_capacity(val.len()),
-            offset: Default::default(),
+            ..Default::default()
         };
         for (name, group) in val.groups() {
             let all = group.all.as_ref().expect("ProxyGroup should have member");
@@ -127,12 +360,12 @@ impl<'a> From<Proxies> for ProxyTree<'a> {
             ret.groups.push(ProxyGroup {
                 _life: PhantomData,
                 name: name.to_owned(),
-                folded: true,
                 proxy_type: group.proxy_type,
                 current,
                 members,
             })
         }
+        ret.groups.sort_by_cached_key(|x| x.name.to_owned());
         ret
     }
 }
@@ -140,6 +373,7 @@ impl<'a> From<Proxies> for ProxyTree<'a> {
 #[derive(Clone, Debug)]
 pub struct ProxyTreeWidget<'a> {
     state: &'a ProxyTree<'a>,
+
     _life: PhantomData<&'a ()>,
 }
 
@@ -154,27 +388,46 @@ impl<'a> ProxyTreeWidget<'a> {
 
 impl<'a> Widget for ProxyTreeWidget<'a> {
     fn render(self, area: tui::layout::Rect, buf: &mut tui::buffer::Buffer) {
+        let cursor = &self.state.cursor;
+        let offset = &self.state.expand_state;
+        let skip = cursor.saturating_sub(2);
         let text = self
             .state
             .groups
             .iter()
-            .map(|x| x.get_widget().lines)
+            .skip(skip)
+            .enumerate()
+            .map(|(i, x)| {
+                x.get_widget(
+                    area.width as usize,
+                    offset.expanded && *cursor == i + skip,
+                    i == cursor - skip,
+                )
+            })
             .reduce(|mut a, b| {
                 a.extend(b);
                 a
             })
             .unwrap_or_default()
             .into_iter()
-            .skip(self.state.offset.y)
             .take(area.height as usize)
             .collect::<Vec<_>>();
 
-        Paragraph::new(text)
-            .block(if self.state.offset.hold {
-                get_focused_block("Proxies")
-            } else {
-                get_block("Proxies")
-            })
-            .render(area, buf);
+        let block = if self.state.expand_state.expanded {
+            get_focused_block("Proxies")
+        } else {
+            get_block("Proxies")
+        };
+
+        let mut inner = block.inner(area);
+        // if area.height > 12 {
+        //     inner.x += 1;
+        //     inner.width -= 2;
+        //     inner.y += 1;
+        //     inner.height -= 2;
+        // }
+        block.render(area, buf);
+
+        Paragraph::new(text).render(inner, buf);
     }
 }
