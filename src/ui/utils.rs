e68c1ac6cb9112953dd5e9d7323fe9f8c7072d5d
George-Miao <gm@miao.dev>
2021-11-14 17:15:19 +0800

    feat: ticks counter

--- src/ui/utils.rs
+++ src/ui/utils.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::VecDeque,
     sync::{mpsc::Sender, Mutex},
     thread::sleep,
     time::{Duration, Instant},
@@ -79,6 +80,63 @@ impl From<model::Level> for Color {
             model::Level::Warning => Color::Yellow,
             model::Level::Error => Color::Red,
         }
+    }
+}
+
+pub struct TicksCounter {
+    ticks: u64,
+    time: Instant,
+    inner: VecDeque<u64>,
+}
+
+impl Default for TicksCounter {
+    fn default() -> Self {
+        Self {
+            time: Instant::now(),
+            ticks: Default::default(),
+            inner: Default::default(),
+        }
+    }
+}
+
+impl TicksCounter {
+    pub fn new_with_time(time: Instant) -> Self {
+        Self {
+            time,
+            ..Self::default()
+        }
+    }
+
+    pub fn new_tick(&mut self) {
+        self.ticks += 1;
+        self.inner.push_front(
+            Instant::now()
+                .duration_since(self.time)
+                .as_millis()
+                .try_into()
+                .expect(
+                    "Hey anyone who sees this as a panic message. Is the universe still there?",
+                ),
+        );
+        if self.inner.len() > 1000 {
+            self.inner.drain(100..);
+        }
+    }
+
+    pub fn tick_rate(&self) -> Option<f64> {
+        if self.inner.len() <= 2 {
+            return None;
+        }
+        let (new, old) = (self.inner.get(0).unwrap(), self.inner.back().unwrap());
+
+        let span_s = (new - old) as f64 / 1000.0;
+
+        // Ticks per Second
+        Some((self.inner.len() as f64) / (span_s as f64))
+    }
+
+    pub fn tick_num(&self) -> u64 {
+        self.ticks
     }
 }
 
