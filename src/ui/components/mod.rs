8b7d4fbae8907febb8f724acedb4ad6424c844ae
George-Miao <gm@miao.dev>
2021-11-25 11:14:37 +0800

    feat: custom sparkline

--- src/ui/components/mod.rs
+++ src/ui/components/mod.rs
@@ -2,13 +2,14 @@ mod block_footer;
 mod constants;
 mod movable_list;
 mod proxy;
+mod sparkline;
 mod tabs;
 mod traffic;
-mod utils;
 
 pub use block_footer::*;
 pub(crate) use constants::*;
 pub(crate) use movable_list::*;
 pub(crate) use proxy::*;
+pub use sparkline::*;
 pub(crate) use tabs::*;
 pub(crate) use traffic::*;
