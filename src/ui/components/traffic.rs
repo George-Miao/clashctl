e5efecdb8220998f133de7fb0bb1d6c844b2f447
George-Miao <gm@miao.dev>
2021-11-25 11:33:19 +0800

    perf: use reversed sparkline

--- src/ui/components/traffic.rs
+++ src/ui/components/traffic.rs
@@ -1,12 +1,19 @@
 use bytesize::ByteSize;
-use tui::style::{Color, Style};
-use tui::widgets::{Sparkline, Widget};
+use tui::widgets::Widget;
 use tui::{
     layout::{Constraint, Layout},
     symbols::bar::Set,
 };
+use tui::{
+    style::{Color, Style},
+    text::Span,
+};
 
-use crate::{define_widget, ui::utils::get_block};
+use crate::{
+    components::{Footer, FooterItem, FooterWidget, Sparkline},
+    define_widget,
+    ui::utils::get_block,
+};
 
 pub const DOTS: Set = Set {
     empty: " ",
@@ -20,12 +27,24 @@ pub const DOTS: Set = Set {
     full: "⣿",
 };
 
+pub const REV_DOTS: Set = Set {
+    empty: " ",
+    one_eighth: "⠁",
+    one_quarter: "⠉",
+    three_eighths: "⠋",
+    half: "⠛",
+    five_eighths: "⠟",
+    three_quarters: "⠿",
+    seven_eighths: "⡿",
+    full: "⣿",
+};
+
+pub const HALF: Constraint = Constraint::Percentage(50);
+
 define_widget!(Traffics);
 
 impl<'a> Widget for Traffics<'a> {
     fn render(self, area: tui::layout::Rect, buf: &mut tui::buffer::Buffer) {
-        let half = Constraint::Percentage(50);
-
         let traffic_size = area.width - 2;
 
         let traffics = self.state.traffics.iter().rev().take(traffic_size.into());
@@ -37,34 +56,42 @@ impl<'a> Widget for Traffics<'a> {
             *down.iter().max().unwrap_or(&100),
         );
 
-        let (up_title, down_title) = (
-            format!("Upload (Max = {}/s)", ByteSize(up_max).to_string_as(true)),
-            format!(
-                "Download (Max = {}/s)",
-                ByteSize(down_max).to_string_as(true)
-            ),
-        );
+        let title = format!("▲ Max = {}/s", ByteSize(up_max).to_string_as(true));
 
         let up_line = Sparkline::default()
             .data(&up)
             .max(up_max)
             .bar_set(DOTS)
-            .block(get_block(&up_title))
             .style(Style::default().fg(Color::Green));
 
         let down_line = Sparkline::default()
             .data(&down)
             .max(down_max)
-            .bar_set(DOTS)
-            .block(get_block(&down_title))
-            .style(Style::default().fg(Color::White));
+            .bar_set(REV_DOTS)
+            .style(Style::default().fg(Color::White))
+            .reversed(true);
+
+        let block = get_block(&title);
+
+        let inner = block.inner(area);
 
         let layout = Layout::default()
             .direction(tui::layout::Direction::Vertical)
-            .constraints([half, half])
-            .split(area);
+            .constraints([HALF, HALF])
+            .split(inner);
 
+        block.render(area, buf);
         up_line.render(layout[0], buf);
         down_line.render(layout[1], buf);
+
+        let mut footer = Footer::default();
+        footer
+            .push_left(FooterItem::span(Span::raw(format!(
+                " ▼ Max = {}/s ",
+                ByteSize(down_max).to_string_as(true)
+            ))))
+            .left_offset(1);
+        let footer_widget = FooterWidget::new(&footer);
+        footer_widget.render(area, buf);
     }
 }
