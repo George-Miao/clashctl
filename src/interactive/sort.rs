bbd4b4eca595737a36818bcbb32f3203eeee5b31
George-Miao <gm@miao.dev>
2021-11-28 19:10:33 +0800

    feat: sort

--- src/interactive/sort.rs
+++ src/interactive/sort.rs
@@ -26,3 +26,41 @@ pub enum SortOrder {
     Ascendant,
     Descendant,
 }
+
+pub struct Noop;
+
+impl<Item> SortMethod<Item> for Noop {
+    #[inline]
+    fn sort_fn(&self, _: &Item, _: &Item) -> Ordering {
+        Ordering::Equal
+    }
+}
+
+impl<'a, T> Sortable<'a, Noop> for T {
+    type Item<'b> = ();
+
+    #[inline]
+    fn sort_with(&mut self, _: Noop) {}
+}
+
+impl<T, F> SortMethod<T> for F
+where
+    F: Fn(&T, &T) -> Ordering,
+{
+    #[inline]
+    fn sort_fn(&self, a: &T, b: &T) -> Ordering {
+        self(a, b)
+    }
+}
+
+impl<'a, T, F> Sortable<'a, F> for Vec<T>
+where
+    F: Fn(&T, &T) -> Ordering,
+{
+    type Item<'b> = T;
+
+    #[inline]
+    fn sort_with(&mut self, method: F) {
+        self.sort_by(method)
+    }
+}
