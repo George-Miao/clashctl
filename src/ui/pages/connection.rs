d88a5afe502679faab6ecffb5bf0ea4175f9e330
George-Miao <gm@miao.dev>
2021-11-29 21:14:10 +0800

    feat: eval connection speed upon update
    Connection speed was evaluated every frame tick. Now it will only be evaluated every servo update.

--- src/ui/pages/connection.rs
+++ src/ui/pages/connection.rs
@@ -8,7 +8,7 @@ use tui::{
 
 use crate::{
     define_widget,
-    model::Connection,
+    model::ConnectionWithSpeed,
     ui::components::{MovableList, MovableListItem},
     ui::HMS,
 };
@@ -21,19 +21,19 @@ impl<'a> Widget for ConnectionPage<'a> {
     }
 }
 
-impl<'a> MovableListItem<'a> for Connection {
+impl<'a> MovableListItem<'a> for ConnectionWithSpeed {
     fn to_spans(&self) -> Spans<'a> {
         let dimmed = Style::default().fg(Color::DarkGray);
         let bolded = Style::default().add_modifier(Modifier::BOLD);
         let (dl, up) = (
-            ByteSize(self.download).to_string_as(true),
-            ByteSize(self.upload).to_string_as(true),
+            ByteSize(self.connection.download).to_string_as(true),
+            ByteSize(self.connection.upload).to_string_as(true),
         );
         let (dl_speed, up_speed) = (
-            ByteSize(self.down_speed().unwrap_or_default()).to_string_as(true) + "/s",
-            ByteSize(self.up_speed().unwrap_or_default()).to_string_as(true) + "/s",
+            ByteSize(self.download.unwrap_or_default()).to_string_as(true) + "/s",
+            ByteSize(self.upload.unwrap_or_default()).to_string_as(true) + "/s",
         );
-        let meta = &self.metadata;
+        let meta = &self.connection.metadata;
         let host = format!("{}:{}", meta.host, meta.destination_port);
 
         let src = format!("{}:{} ", meta.source_ip, meta.source_port);
@@ -48,7 +48,7 @@ impl<'a> MovableListItem<'a> for Connection {
         );
         let dash: String = "─".repeat(44_usize.saturating_sub(src.len() + dest.len()).max(1));
 
-        let time = (Utc::now() - self.start).hms();
+        let time = (Utc::now() - self.connection.start).hms();
         vec![
             Span::styled(format!("{:45}", host), bolded),
             // Download size
@@ -68,7 +68,7 @@ impl<'a> MovableListItem<'a> for Connection {
             Span::raw(format!("{:10}", time)),
             // Rule
             Span::styled(" ✤  ", dimmed),
-            Span::raw(format!("{:15}", self.rule)),
+            Span::raw(format!("{:15}", self.connection.rule)),
             // IP
             Span::styled(" ⇄  ", dimmed),
             Span::raw(src),
@@ -76,7 +76,7 @@ impl<'a> MovableListItem<'a> for Connection {
             Span::raw(dest),
             // Chain
             Span::styled("   ⟴  ", dimmed),
-            Span::raw(self.chains.join(" - ")),
+            Span::raw(self.connection.chains.join(" - ")),
         ]
         .into()
     }
