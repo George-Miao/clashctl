06ff00b89524ba3adb379219cdc9d2b98beca024
George Miao <gm@miao.dev>
2021-12-06 21:39:46 +0800

    fix: get_version for premium core

--- clashctl/src/model/mod.rs
+++ clashctl/src/model/mod.rs
@@ -29,16 +29,34 @@ pub struct Delay {
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 #[serde(rename_all = "lowercase")]
 pub struct Version {
-    pub version: VersionType,
+    // Clash Premium only
+    pub premium: Option<bool>,
+    pub version: VersionPayload,
 }
 
 cfg_if! {
     if #[cfg(feature = "deserialize")] {
         use chrono::{Utc, DateTime};
         pub type TimeType = DateTime<Utc>;
-        pub type VersionType = semver::Version;
     } else {
         pub type TimeType = String;
-        pub type VersionType = String;
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[serde(untagged, rename_all = "lowercase")]
+pub enum VersionPayload {
+    #[cfg(feature = "deserialize")]
+    SemVer(semver::Version),
+    Raw(String),
+}
+
+impl ToString for VersionPayload {
+    fn to_string(&self) -> String {
+        match self {
+            #[cfg(feature = "deserialize")]
+            VersionPayload::SemVer(ver) => ver.to_string(),
+            VersionPayload::Raw(content) => content.to_owned(),
+        }
     }
 }
