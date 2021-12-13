c20208dc1cd709fb56d380f53c2d9cda50af1819
George Miao <gm@miao.dev>
2021-12-13 10:17:17 +0800

    fix(tui): clap breaking change

--- clashctl-tui/src/tui_opt.rs
+++ clashctl-tui/src/tui_opt.rs
@@ -2,7 +2,8 @@ use smart_default::SmartDefault;
 
 #[derive(Debug, SmartDefault, clap::Parser)]
 pub struct TuiOpt {
-    #[clap(about = "Interval between requests", default_value = "5")]
+    #[clap(default_value = "5")]
     #[default = 5.0]
+    /// Interval between requests
     pub interval: f32,
 }
